42db49a1d4c21a0c4815f31b8c1ee8761716a74f
bayous3 <sebia.baya@outlook.com>
2023-12-27 15:53:07 +0100

    modif img

--- scripts/templates/PhotographerPhoto.js
+++ scripts/templates/PhotographerPhoto.js
@@ -13,7 +13,13 @@ class PhotographerPhoto {
         <a href="#" aria-label="image">
           <img alt="${this.photo.title}" src="${this.photo.image}">
         </a>
-        <figcaption>${this.photo.title} </figcaption>
+        <figcaption>
+          <p>${this.photo.title}</p> 
+          <span class="like like-${this.photo.id}">
+                        <span id="like-${this.photo.id}">${this.photo.likes}</span>
+                        <span class="like-coeur" aria-label="likes"></span>
+                    </span>
+        </figcaption>
       </figure>
       `;
 
